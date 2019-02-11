7dc8756d020d7a415647645624cab588f694a01b
Anurag Anand <anuraganand789@hotmail.com>
2019-02-11 22:46:49 +0530

    reading max X-Y coordinates and adding support for cursor movement in the scratch pad

--- scratch.c
+++ scratch.c
@@ -8,8 +8,31 @@ void main() {
   noecho();
   keypad(stdscr, TRUE);
 
+  int MAX_X = 0;
+  int MAX_Y = 0;
+  getmaxyx(stdscr, MAX_Y, MAX_X);
+  printw("Max x : %d, Max y : %d \n", MAX_X, MAX_Y);
+
+  int cur_x = 0;
+  int cur_y = 0;
+  
   while((ch = getch()) != KEY_F(1)) {
-    printw("%c", ch);
+    switch(ch) {
+    case KEY_BACKSPACE :
+      delch();
+      break;
+    case KEY_LEFT :
+      
+          case KEY_LEFT :
+	        case KEY_LEFT :
+	        case KEY_LEFT :
+    }
+    
+    if(ch == KEY_BACKSPACE) {
+      delch();
+    } else {
+      printw("%c", ch);
+    }
   }
   
   endwin();
